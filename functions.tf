fbf047a84c349aecd7221ee059a3c50a794f264d
Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>
2023-08-15 18:03:20 -0700

    Update auth function

--- functions.tf
+++ functions.tf
@@ -22,14 +22,14 @@ module "lambda_function_auth" {
   timeout       = 300
   source_path   = "${path.module}/src/auth/"
   function_name = "authorizer"
-  handler       = "app.handler"
+  handler       = "app.auth_handler"
   runtime       = "python3.9"
+  publish       = true
   create_sam_metadata = true
-  # publish       = true
-  # allowed_triggers = {
-  #   APIGatewayAny = {
-  #     service    = "apigateway"
-  #     source_arn = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.api.id}/*/*/*"
-  #   }
-  # }
+    allowed_triggers = {
+      APIGatewayAny = {
+        service    = "apigateway"
+        source_arn = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.api.id}/*/*/*"
+      }
+    }
 }
